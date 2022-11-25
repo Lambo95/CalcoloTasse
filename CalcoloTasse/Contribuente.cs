f81cd8c7c0015389dce9ea8af7fdf7579b6240c4
Lambo95 <lamberto.martini95@gmail.com>
2022-11-25 16:59:04 +0100

    Aggiungere i file di progetto.

---
+++ CalcoloTasse/Contribuente.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcoloTasse
+{
+    internal class Contribuente
+    {
+        public struct AliquotaPercentuale
+        {
+
+            public const double ScaglionePercentuale1 = 0.23;
+            public const double ScaglionePercentuale2 = 0.27;
+            public const double ScaglionePercentuale3 = 0.38;
+            public const double ScaglionePercentuale4 = 0.41;
+            public const double ScaglionePercentuale5 = 0.43;
+        }
+        public struct AliquotaInt
+        {
+
+            public const int ScaglioneInt2 = 3450;
+            public const int ScaglioneInt3 = 6960;
+            public const int ScaglioneInt4 = 17220;
+            public const int ScaglioneInt5 = 25420;
+        }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public DateTime DataNascita { get; set; }
+
+        public string CodiceFiscale { get; set; }
+        public char Sesso { get; set; }
+        public string ComuneResidenza { get; set; }
+        public double RedditoAnnuale { get; set; }
+
+        public Contribuente(string nome, string cognome, DateTime dataNascita, string codiceFiscale, char sesso, string comuneResidenza, double redditoAnnuale)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            DataNascita = dataNascita;
+            CodiceFiscale = codiceFiscale;
+            Sesso = sesso;
+            ComuneResidenza = comuneResidenza;
+            RedditoAnnuale = redditoAnnuale;
+        }
+
+        public void CalcoloReddito()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            if (RedditoAnnuale < 15000)
+            {
+                double imposta = RedditoAnnuale * AliquotaPercentuale.ScaglionePercentuale1;
+                Console.WriteLine($"CALCOLO DELL’IMPOSTA DA VERSARE:\r\n\r\nContribuente:{Nome} {Cognome},\r\nnato il {DataNascita} {Sesso},\r\nresidente in {ComuneResidenza},\r\ncodice fiscale: {CodiceFiscale}\r\n\r\nReddito dichiarato: {RedditoAnnuale:C}\r\n\r\nIMPOSTA DA VERSARE:  {imposta:C}");
+            }
+            else if (RedditoAnnuale > 15001 && RedditoAnnuale < 28000)
+            {
+                double imposta = ((RedditoAnnuale - 15000) * AliquotaPercentuale.ScaglionePercentuale2) + AliquotaInt.ScaglioneInt2;
+                Console.WriteLine($"CALCOLO DELL’IMPOSTA DA VERSARE:\r\n\r\nContribuente:{Nome} {Cognome},\r\nnato il {DataNascita.ToString("dd/MM/yyyy")} {Sesso},\r\nresidente in {ComuneResidenza},\r\ncodice fiscale: {CodiceFiscale}\r\n\r\nReddito dichiarato: {RedditoAnnuale:C}\r\n\r\nIMPOSTA DA VERSARE:  {imposta:C}");
+
+            }
+            else if (RedditoAnnuale > 28001 && RedditoAnnuale < 55000)
+            {
+                double imposta = ((RedditoAnnuale - 28000) * AliquotaPercentuale.ScaglionePercentuale3) + AliquotaInt.ScaglioneInt3;
+                Console.WriteLine($"CALCOLO DELL’IMPOSTA DA VERSARE:\r\n\r\nContribuente:{Nome} {Cognome},\r\nnato il {DataNascita.ToString("dd/MM/yyyy")} {Sesso},\r\nresidente in {ComuneResidenza},\r\ncodice fiscale: {CodiceFiscale}\r\n\r\nReddito dichiarato: {RedditoAnnuale:C}\r\n\r\nIMPOSTA DA VERSARE:  {imposta:C}");
+
+            }
+            else if (RedditoAnnuale > 55001 && RedditoAnnuale < 75000)
+            {
+                double imposta = ((RedditoAnnuale - 55000) * AliquotaPercentuale.ScaglionePercentuale4) + AliquotaInt.ScaglioneInt4;
+                Console.WriteLine($"CALCOLO DELL’IMPOSTA DA VERSARE:\r\n\r\nContribuente:{Nome} {Cognome},\r\nnato il {DataNascita.ToString("dd/MM/yyyy")} {Sesso},\r\nresidente in {ComuneResidenza},\r\ncodice fiscale: {CodiceFiscale}\r\n\r\nReddito dichiarato: {RedditoAnnuale:C}\r\n\r\nIMPOSTA DA VERSARE:  {imposta:C}");
+
+            }
+            else if (RedditoAnnuale > 75001)
+            {
+                double imposta = ((RedditoAnnuale - 75000) * AliquotaPercentuale.ScaglionePercentuale5) + AliquotaInt.ScaglioneInt5;
+                Console.WriteLine($"CALCOLO DELL’IMPOSTA DA VERSARE:\r\n\r\nContribuente:{Nome} {Cognome},\r\nnato il {DataNascita.ToString("dd/MM/yyyy")} {Sesso},\r\nresidente in {ComuneResidenza},\r\ncodice fiscale: {CodiceFiscale}\r\n\r\nReddito dichiarato: {RedditoAnnuale:C}\r\n\r\nIMPOSTA DA VERSARE: &euro {imposta}");
+
+            }
+
+        }
+    }
+}
---
+++ CalcoloTasse/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CalcoloTasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Esegui();
+        }
+
+        public static void Esegui()
+        {
+            Console.WriteLine("\n-------------------------------------------------------");
+            Console.WriteLine("\n************** Portale calcolo imposte  *****************");
+            Console.WriteLine("\n----------------------- V.1 ---------------------------");
+            Console.WriteLine("\n \t ************** MENU ****************");
+            Console.WriteLine("0 - per uscire");
+            Console.WriteLine("1 -  Calcola imposte contribuente");
+            Console.WriteLine("\n-------------------------------------------------------");
+
+
+            switch (Console.ReadLine())
+            {
+                case "0":
+                    Environment.Exit(0);
+                    break;
+                case "1":
+                    CreaContribuente();
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("\t\t\n => Digita 0 o 1!!!!!");
+                    Esegui();
+                    break;
+            }
+        }
+
+        public static void CreaContribuente()
+        {
+            try
+            {
+                Console.WriteLine("\n===============  Inserisci contribuente  ==============");
+                Console.WriteLine("\nSeguire i seguenti passaggi");
+                Console.WriteLine("\nDigita il tuo nome: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("\nDigita il tuo cognome: ");
+                string cognome = Console.ReadLine();
+                Console.WriteLine("\nDigita la tua data di nascita: ");
+                string dataNascita = Console.ReadLine();
+                var dataNascitaParse = DateTime.Parse(dataNascita);
+                Console.WriteLine("\nDigita il tuo codice fiscale: ");
+                string codiceFiscale = Console.ReadLine();
+                Console.WriteLine("\nDigita M o F per il sesso: ");
+                char sesso = char.Parse(Console.ReadLine());
+                Console.WriteLine("\nDigita il tuo comune di residenza: ");
+                string comuneResidenza = Console.ReadLine();
+                Console.WriteLine("\nDigita il tuo reddito annuale: ");
+                double redditoAnnuale = double.Parse(Console.ReadLine());
+
+                Contribuente c = new Contribuente(nome, cognome, dataNascitaParse, codiceFiscale, sesso, comuneResidenza, redditoAnnuale);
+
+                Console.WriteLine("\n\t\tCalcolo tasse in corso....");
+                Thread.Sleep(2000);
+                c.CalcoloReddito();
+                Console.WriteLine("================= Fine programma... ===============");
+                Console.WriteLine("\n \t ************** MENU ****************");
+                Console.WriteLine("\n0 - per uscire");
+                Console.WriteLine("1 - Calcola imposte nuovo contribuente");
+                Console.WriteLine("\n-------------------------------------------------------");
+
+
+                switch (Console.ReadLine())
+                {
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    case "1":
+                        CreaContribuente();
+                        break;
+                    default:
+                        Console.WriteLine("Digita 0 o 1");
+                        Esegui();
+                        break;
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine($"Errore:{ex.Message}");
+                Console.WriteLine("\n\tInput non corretto... \n\tRiavvio programma....");
+                Thread.Sleep(3000);
+
+                Console.Clear();
+                Esegui();
+            }
+        }
+    }
+}
